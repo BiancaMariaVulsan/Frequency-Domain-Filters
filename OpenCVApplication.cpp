5bb95536f522639b33573b06a993d00089196aef
Vulsan Bianca Maria <68532670+BiancaMariaVulsan@users.noreply.github.com>
2023-06-02 01:01:12 +0300

    generate video added

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -69,8 +69,8 @@ Mat frequency_domain_filter(Mat src, int nr, int val) {
 				int dist = (i - fourier.rows / 2) * (i - fourier.rows / 2) + (j - fourier.cols / 2) * (j - fourier.cols / 2);
 				if (dist > R * R)
 				{
-					//channels[0].at<float>(i, j) = 0;
-					//channels[1].at<float>(i, j) = 0;
+					channels[0].at<float>(i, j) = 0;
+					channels[1].at<float>(i, j) = 0;
 					mag.at<float>(i, j) = 0;
 				}
 			}
@@ -86,8 +86,8 @@ Mat frequency_domain_filter(Mat src, int nr, int val) {
 				int dist = (i - fourier.rows / 2) * (i - fourier.rows / 2) + (j - fourier.cols / 2) * (j - fourier.cols / 2);
 				if (dist < R * R)
 				{
-					//channels[0].at<float>(i, j) = 0;
-					//channels[1].at<float>(i, j) = 0;
+					channels[0].at<float>(i, j) = 0;
+					channels[1].at<float>(i, j) = 0;
 					mag.at<float>(i, j) = 0;
 				}
 			}
@@ -102,8 +102,8 @@ Mat frequency_domain_filter(Mat src, int nr, int val) {
 			{
 				float expon = exp(-((i - fourier.rows / 2) * (i - fourier.rows / 2) + (j - fourier.cols / 2) * (j - fourier.cols / 2)) / (A * A));
 
-				//channels[0].at<float>(i, j) *= expon;
-				//channels[1].at<float>(i, j) *= expon;
+				channels[0].at<float>(i, j) *= expon;
+				channels[1].at<float>(i, j) *= expon;
 				mag.at<float>(i, j) *= expon;
 
 			}
@@ -125,31 +125,34 @@ Mat frequency_domain_filter(Mat src, int nr, int val) {
 			}
 		}
 	}
-
+	// Alternattive solution, in case we don't modify directly the channels for each filter and we modify just the magnitude
 	//for (int i = 0; i < mag.rows; i++) {
 	//	for (int j = 0; j < mag.cols; j++) {
 	//		channels[0].at<float>(i, j) = mag.at<float>(i,j) * cos(phi.at<float>(i,j));
 	//		channels[1].at<float>(i, j) = mag.at<float>(i, j) * sin(phi.at<float>(i, j));
 	//	}
 	//}
+
 	Mat mag2(mag.rows, mag.cols, CV_32FC1);
 	for (int i = 0; i < mag.rows; i++) {
 		for (int j = 0; j < mag.cols; j++) {
 			mag2.at<float>(i, j) = log2(mag.at<float>(i, j) + 1);
 		}
 	}
-	Mat_<uchar> mag_normalized(mag.rows, mag.cols);
 	float maxLog = log2(max + 1);
 
-	for (int i = 0; i < mag.rows; i++) {
-		for (int j = 0; j < mag.cols; j++) {
-			mag_normalized(i, j) = mag2.at<float>(i, j) * 255 / maxLog;
+	Mat_<uchar> mag_normalized(mag.rows, mag.cols);
+
+	if (nr == 3) {
+		for (int i = 0; i < mag.rows; i++) {
+			for (int j = 0; j < mag.cols; j++) {
+				mag_normalized(i, j) = mag2.at<float>(i, j) * 255 / maxLog;
+			}
 		}
 	}
-
-	//normalize(mag2, mag_normalized, 0, 255, NORM_MINMAX, CV_8UC1);
-	//imshow("Magnitude", mag_normalized);
-	//waitKey(0);
+	else {
+		normalize(mag2, mag_normalized, 0, 255, NORM_MINMAX, CV_8UC1);
+	}
 
 	//perform inverse transform and put results in dstf
 	Mat dst, dstf;
@@ -176,44 +179,89 @@ Mat frequency_domain_filter(Mat src, int nr, int val) {
 	return final;
 }
 
+int genVideo(Mat_<uchar> img, int minR, int maxR, int nr) {
+
+	// Create a VideoCapture object and use camera to capture the video
+	VideoCapture cap(0);
+
+	// Check if camera opened successfully
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream" << endl;
+		return -1;
+	}
+
+	// Default resolutions of the frame are obtained.The default resolutions are system dependent.
+	//int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+	//int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+	// Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
+	VideoWriter video("output.mp4", VideoWriter::fourcc('H', '2', '6', '4'), 10, Size(512, 256));
+
+	for (int frameNumber = minR; frameNumber < maxR; frameNumber+=2) {
+
+		Mat frame = frequency_domain_filter(img, nr, frameNumber);
+
+		// Capture frame-by-frame
+		//cap >> frame;
+
+		// If the frame is empty, break immediately
+		if (frame.empty())
+			break;
+
+		// Write the frame into the file 'outcpp.avi'
+		video.write(frame);
+
+		// Display the resulting frame    
+		//imshow("Frame", frame);
+	}
+
+	// When everything done, release the video capture and write object
+	//cap.release();
+	video.release();
+
+	// Closes all the frames
+	destroyAllWindows();
+	return 0;
+}
+
 int main()
 {
 	int option;
+	cout << " 0 - regenerate images low pass\n 1 - regenerate images for high pass\n 2 - regenerate images for gaussian low pass\n 3 - regenerate images for gaussian high pass\n";
+	cout << " 4 - generate video for low pass\n 5 - generate video for high pass\n 6 - generate video for gaussian low pass\n 7 - generate video for gaussian high pass\n";
 	cin >> option;
+
 	Mat_<uchar> img = imread("Images/cameraman.bmp", IMREAD_GRAYSCALE);
 
 	switch (option)
 	{
+	case 0:
+		for (int v = 10; v <= 80; v += 2) {
+			imwrite(std::string("Images/low_pass/low_pass_"+ to_string(v)+".bmp"), frequency_domain_filter(img, 0, v));
+		}
+		break;
 	case 1:
 		for (int v = 10; v <= 80; v += 2) {
-			//imshow("Low Pass Filter Result", frequency_domain_filter(img, 0, v));
-			//waitKey(0);
-			imwrite(std::string("Images/low_pass/low_pass_"+ std::to_string(v)+".bmp"), frequency_domain_filter(img, 0, v));
+			imwrite("Images/high_pass/high_pass_" + to_string(v) + ".bmp", frequency_domain_filter(img, 1, v));
 		}
 		break;
 	case 2:
 		for (int v = 10; v <= 80; v += 2) {
-			//imshow("High Pass Filter Result", frequency_domain_filter(img, 1, v));
-			//waitKey(0);
-			imwrite("Images/high_pass/high_pass_" + std::to_string(v) + ".bmp", frequency_domain_filter(img, 1, v));
+			imwrite("Images/gaussian_low_pass/gaussian_low_pass_" + to_string(v) + ".bmp", frequency_domain_filter(img, 2, v));
 		}
 		break;
 	case 3:
 		for (int v = 10; v <= 80; v += 2) {
-			//imshow("Gaussian Low Pass Filter Result", frequency_domain_filter(img, 2, v));
-			//waitKey(0);
-			imwrite("Images/gaussian_low_pass/gaussian_low_pass_" + std::to_string(v) + ".bmp", frequency_domain_filter(img, 2, v));
-		}
-		break;
-	case 4:
-		for (int v = 10; v <= 80; v += 2) {
-			//imshow("Gaussian High Pass Filter Result", frequency_domain_filter(img, 3, v));
-			//waitKey(0);
-			imwrite("Images/gaussian_high_pass/gaussian_high_pass_v222" + std::to_string(v) + ".bmp", frequency_domain_filter(img, 3, v));
+			imwrite("Images/gaussian_high_pass/gaussian_high_pass_v222" + to_string(v) + ".bmp", frequency_domain_filter(img, 3, v));
 		}
 		break;
 	default:
-		break;
+		if (option == 4 || option == 5 || option == 6 || option == 7) {
+			genVideo(img, 10, 80, option - 4);
+		}
+		else {
+			cout << "Invalid number for the option\n";
+		}
 	}
 	return 0;
 }
